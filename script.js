1f552c089e460e26d7f9e111489bd4f2ddf4d211
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-03-30 22:21:21 -0400

    I'm having to call it a day at this point. I have struggled with COVID symptoms the last three days have been agonizing. all I could do to get through classes yesterday and today. I must rest and attempt this homework at a later time. final commit message before deadline.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var searchForm = $("search-form");
-var searchTerm = $("#search-term");
+var searchTermEl = $("#search-term");
 
 
 
 
-searchForm.on("sumbit", function(){
-
+searchForm.on("submit", function(event) {
+    event.preventDefault();
     // grab the search term out of the input.
+    var searchTermEl = searchTermEl.val();
+    console.log(searchTerm);
+  
     // build API url with search term and the API key
     // store the API key in a variable
     // make and API call using fetch
     // convert the response from JSON
     // start by console logging the data.
-    
-})
+
+});
