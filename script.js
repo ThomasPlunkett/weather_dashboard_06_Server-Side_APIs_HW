a4900646b675df0b1f4ea1a7ed169670c8916749
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-03-30 22:06:10 -0400

    psuedo coding process for completing search form with API

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 var searchForm = $("search-form");
-var searchTerm= $(#search-term);
+var searchTerm = $("#search-term");
+
 
 
 
 searchForm.on("sumbit", function(){
 
     // grab the search term out of the input.
+    // build API url with search term and the API key
+    // store the API key in a variable
+    // make and API call using fetch
+    // convert the response from JSON
+    // start by console logging the data.
+    
 })
